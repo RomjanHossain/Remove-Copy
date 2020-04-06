fbff5a4492f48c6d1391e47378c053b734af9e23
Romjan D. Hossain <47692261+RomjanHossain@users.noreply.github.com>
2020-04-06 15:10:39 +0600

    Create getvideoLength-unix.py
    Get video length unix

---
+++ getvideoLength-unix.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+currentDir = os.getcwd()
+o = 0
+getallfiles = [x for a, b, c in os.walk(currentDir) for x in c]
+# print(len(getalldir))
+with open('files.txt', 'w') as vi:
+    for file in getallfiles:
+
+        NewFile = file.replace(' ', '\ ')
+        n2 = NewFile.replace('(', '\(')
+        n3 = n2.replace(')', '\)')
+        command = f'exiftool {n3} | grep Duration'
+        # print(command)
+        f = os.popen(command)
+        length = f.read()[:41][-7:]
+        # print(f'{file} \n{length}')
+
+        vi.write(f'{file} \n{length}\n')
